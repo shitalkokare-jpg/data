962de2da85e5ccabd170f23e28042be28577898e
shitalkokare <shital.kokare@vpkbiet.org>
2026-08-06 14:09:51 +0530

    add

---
+++ add.py
@@ -0,0 +1,10 @@
+# Store input numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the two numbers
+total = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {total}")
+
